824c861c788e42ac667f32dce37893caf47f06d4
vinicius-yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2023-10-31 16:51:43 -0300

    Adicionado model de novo compromisso na main activity

--- app/src/main/java/com/agenda/MainActivity.java
+++ app/src/main/java/com/agenda/MainActivity.java
@@ -1,14 +1,30 @@
 package com.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import java.util.Observable;
+import java.util.Observer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Observer {
+
+
+    private NovoCompromissoModel novoCompromissoModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        novoCompromissoModel = new NovoCompromissoModel();
+        novoCompromissoModel.addObserver(this);
+
+        // TODO pegar os componentes da tela
+    }
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+        // TODO atualizar dados na tela quando os dados do model mudar
     }
 }
