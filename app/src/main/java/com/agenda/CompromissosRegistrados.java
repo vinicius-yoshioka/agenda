ffb38e66f314ab1096394318edd80d6ee197ed60
vinicius-yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2023-10-31 23:28:50 -0300

    Criado fragmentos da tela

---
+++ app/src/main/java/com/agenda/CompromissosRegistrados.java
@@ -0,0 +1,61 @@
+package com.agenda;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CompromissosRegistrados extends Fragment {
+
+
+    Button botao_compromissos_hoje;
+    Button botao_compromissos_outraData;
+    TextView texto_compromissosCadastrados;
+
+
+    public CompromissosRegistrados() {}
+
+
+    public static CompromissosRegistrados newInstance() {
+        CompromissosRegistrados fragment = new CompromissosRegistrados();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compromissos_registrados, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        botao_compromissos_hoje = view.findViewById(R.id.botao_compromissos_hoje);
+        botao_compromissos_outraData = view.findViewById(R.id.botao_compromissos_outraData);
+        texto_compromissosCadastrados = view.findViewById(R.id.texto_compromissosCadastrados);
+
+        botao_compromissos_outraData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirDatePicker(v);
+            }
+        });
+    }
+
+
+    public void abrirDatePicker(View v) {
+        DialogFragment newFragment = new AgendaDatePicker();
+        newFragment.show(getChildFragmentManager(), "datePicker");
+    }
+}
---
+++ app/src/main/java/com/agenda/NovoCompromisso.java
@@ -0,0 +1,75 @@
+package com.agenda;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NovoCompromisso extends Fragment {
+
+
+    Button botao_novoCompromisso_data;
+    Button botao_novoCompromisso_hora;
+    EditText texto_novoCompromisso_descricao;
+    Button botao_novoCompromisso_ok;
+
+
+    public NovoCompromisso() {}
+
+
+    public static NovoCompromisso newInstance() {
+        NovoCompromisso fragment = new NovoCompromisso();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_novo_compromisso, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        botao_novoCompromisso_data = view.findViewById(R.id.botao_novoCompromisso_data);
+        botao_novoCompromisso_hora = view.findViewById(R.id.botao_novoCompromisso_hora);
+        texto_novoCompromisso_descricao = view.findViewById(R.id.texto_novoCompromisso_descricao);
+        botao_novoCompromisso_ok = view.findViewById(R.id.botao_novoCompromisso_ok);
+
+        botao_novoCompromisso_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirDatePicker(v);
+            }
+        });
+
+        botao_novoCompromisso_hora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirTimePicker(v);
+            }
+        });
+    }
+
+
+    public void abrirDatePicker(View v) {
+        DialogFragment newFragment = new AgendaDatePicker();
+        newFragment.show(getChildFragmentManager(), "datePicker");
+    }
+
+    public void abrirTimePicker(View v) {
+        DialogFragment newFragment = new AgendaTimePicker();
+        newFragment.show(getChildFragmentManager(), "timePicker");
+    }
+}
