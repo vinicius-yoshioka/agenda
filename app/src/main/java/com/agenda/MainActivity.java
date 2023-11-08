0e72b639cb68e69f09b8adb46cfb99bf7cf2e79c
vinicius-yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2023-11-07 21:48:59 -0300

    removido models da main activity

--- app/src/main/java/com/agenda/MainActivity.java
+++ app/src/main/java/com/agenda/MainActivity.java
@@ -2,29 +2,12 @@ package com.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.Observable;
-import java.util.Observer;
 
-public class MainActivity extends AppCompatActivity implements Observer {
-
-
-    private CompromissosRegistradosModel compromissosRegistradosModel;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        compromissosRegistradosModel = new CompromissosRegistradosModel();
-        compromissosRegistradosModel.addObserver(this);
-
-        // TODO pegar os componentes da tela
-    }
-
-
-    @Override
-    public void update(Observable o, Object arg) {
-        // TODO atualizar dados na tela quando os dados do model mudar
     }
 }
