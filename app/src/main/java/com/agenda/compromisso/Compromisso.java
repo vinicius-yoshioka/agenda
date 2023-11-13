50a240f447d72a7014e9886b25d2a53b44cf477b
vinicius-yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2023-11-13 20:27:15 -0300

    Alterado tipo de data e hora do Compromisso para Data e Hora

--- app/src/main/java/com/agenda/compromisso/Compromisso.java
+++ app/src/main/java/com/agenda/compromisso/Compromisso.java
@@ -1,28 +1,30 @@
-package com.agenda;
+package com.agenda.compromisso;
 
 import androidx.annotation.Nullable;
+import com.agenda.Date.Data;
+import com.agenda.Hora.Hora;
 
 public class Compromisso {
 
 
-    private @Nullable String data;
-    private @Nullable String hora;
+    private @Nullable Data data;
+    private @Nullable Hora hora;
     private @Nullable String descricao;
 
 
-    public @Nullable String getData() {
+    public @Nullable Data getData() {
         return data;
     }
 
-    public void setData(@Nullable String data) {
+    public void setData(@Nullable Data data) {
         this.data = data;
     }
 
-    public @Nullable String getHora() {
+    public @Nullable Hora getHora() {
         return hora;
     }
 
-    public void setHora(@Nullable String hora) {
+    public void setHora(@Nullable Hora hora) {
         this.hora = hora;
     }
 
